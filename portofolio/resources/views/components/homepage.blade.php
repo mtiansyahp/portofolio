de91f64503eb0071f9e598206161ea27e44c85e1
mtiansyahpratamaa <53297603+mtiansyahp@users.noreply.github.com>
2022-09-21 22:59:20 +0700

    push for content fix typo

--- portofolio/resources/views/components/homepage.blade.php
+++ portofolio/resources/views/components/homepage.blade.php
@@ -2,5 +2,5 @@
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="mr-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a> -->
       <h2>I'm a <span>Website Developer</span></span></h2>
-      <h2>with experiences on several projects focusing on website development.</h2>
-      <h2>i'm use my skill to solve Frontend and Backend problem in IT case.</h2>
+      <h2>with experience on several projects focusing on website development.</h2>
+      <h2>i use my skills to solve Frontend and Backend problem in IT case.</h2>
